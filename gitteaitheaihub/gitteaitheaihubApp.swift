0fb690e5b04764fcb003d09d61c1aa6ec2aa6f53
terrablade12 <terrablade1212@gmail.com>
2026-07-04 14:37:09 +0800

    Initial Commit

---
+++ gitteaitheaihub/gitteaitheaihubApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitteaitheaihubApp.swift
+//  gitteaitheaihub
+//
+//  Created by Iain Tan Qi yuan on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct gitteaitheaihubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
