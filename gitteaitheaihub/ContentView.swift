03a54ee8983ac18f7c1a95edd0cad7c389a3c08f
terrablade12 <terrablade1212@gmail.com>
2026-07-04 14:46:01 +0800

    I made tje backgroupnd and the text

--- gitteaitheaihub/ContentView.swift
+++ gitteaitheaihub/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            
+            LinearGradient(colors: [.green, .blue],
+                                            startPoint: .topTrailing,
+                                            endPoint: .bottomTrailing)
+            VStack {
+
+                Text("IAIN")
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundStyle(LinearGradient(colors: [.white, .yellow],
+                                                    startPoint: .topTrailing,
+                                                    endPoint: .bottomTrailing))
+            }
+            
         }
-        .padding()
+        .ignoresSafeArea()
+        
+
+        
     }
 }
 
