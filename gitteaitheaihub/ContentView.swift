7e7fb5c4ae6a313a79899e317e7938e0fe8db1d3
terrablade12 <terrablade1212@gmail.com>
2026-07-11 14:13:55 +0800

    jcsajldsa new version still with exra name

--- gitteaitheaihub/ContentView.swift
+++ gitteaitheaihub/ContentView.swift
@@ -10,46 +10,40 @@ import SwiftUI
 struct ContentView: View {
     @State private var isAnimating = false
     @State private var offset = CGSize.zero
-    @State private var isAnimating2 = false
-    @State private var offset2 = CGSize.zero
+
     @State private var rotationAngle = 0.0
     @State private var spring = 0.0
+
     @State private var scale = 1.0
     
     var body: some View {
         
         
         ZStack{
-            
-            LinearGradient(colors: [.green, .blue],
-                           startPoint: .topTrailing,
-                           endPoint: .bottomTrailing)
+            if isAnimating {
+                LinearGradient(colors: [.green, .blue],
+                               startPoint: .topTrailing,
+                               endPoint: .bottomTrailing)
+            } else {
+                LinearGradient(colors: [.red, .yellow],
+                               startPoint: .topTrailing,
+                               endPoint: .bottomTrailing)
+            }
             VStack {
                 Text("Im ezraa")
                 Text("DRAG THE CIRCLE")
                     .bold()
                     .font(.largeTitle)
-                    .foregroundStyle(isAnimating2 ? LinearGradient(colors: [.white, .yellow],
+                    .animation(.bouncy)
+                    .foregroundStyle(isAnimating ? LinearGradient(colors: [.white, .yellow],
                                                        startPoint: .topTrailing,
                                                        endPoint: .bottomTrailing) : LinearGradient(colors: [.blue, .black],
                                                                                                    startPoint: .topTrailing,
                                                                                                    endPoint: .bottomTrailing))
                 
 
-                    .offset(offset2)
-                    .gesture(
-                        DragGesture()
-                            .onChanged { value in
-                                self.offset2 = value.translation
-                                isAnimating2 = true
-                            }
-                            .onEnded { _ in
-                                withAnimation {
-                                    self.offset2 = .zero
-                                    isAnimating2 = false
-                                }
-                            }
-                    )
+                    
+                    
                 Circle()
                 
                     .fill(isAnimating ? LinearGradient(colors: [.yellow, .white],
